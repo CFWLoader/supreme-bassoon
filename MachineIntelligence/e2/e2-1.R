507a91b47ecd26c08f2f18d1ceda02db97e28858
cfwloader <cfwloader@gmail.com>
2018-04-02 20:49:58 +0800

    Now can plot the scatters.

--- MachineIntelligence/e2/e2-1.R
+++ MachineIntelligence/e2/e2-1.R
@@ -1,34 +1,12 @@
+library(ggplot2)
+
 # Set working dir to this file's path.
-script.dir <- dirname(sys.frame(1)$ofile)
+script.dir = dirname(sys.frame(1)$ofile)
 
 setwd(script.dir)
 
-dataset <- read.csv("../datasets/applesOranges.csv")
+dataset = read.csv("../datasets/applesOranges.csv")
 
-class1 = data.frame()
+ggplot(dataset, aes(x = x.1, y = x.2, color=y)) + geom_point()
 
-class2 = data.frame()
-
-for(idx in c(1:length(dataset)))
-{
-    if(dataset$y[idx] == 0)
-    {
-        class1 = rbind(class1, dataset[idx,])
-    }
-    else
-    {
-        class2 = rbind(class2, dataset[idx,])
-    }
-}
-
-# print(class1)
-
-# print(class2)
-
-# print(head(dataset))
-
-png("./applesOranges.png")
-
-plot(x = dataset$x.1, y = dataset$x.2)
-
-dev.off()
+ggsave("./applesOranges.png")
