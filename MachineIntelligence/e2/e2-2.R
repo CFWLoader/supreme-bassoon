80ffc555dff4671788de8ea1e7fc1294d0397fe0
cfwloader <cfwloader@gmail.com>
2018-04-07 19:57:15 +0800

    Now can solve H2.

--- MachineIntelligence/e2/e2-2.R
+++ MachineIntelligence/e2/e2-2.R
@@ -25,98 +25,71 @@ mlp = function(x, w, a, b, activation = tanh)
 }
 
 # Input.
+run_test_and_plot = function (input_x, num_n_hid, num_mlps, a_stddev, file_path)
+{
+    dataset = NULL
+
+    err = NULL
+
+    min_err = NULL
+
+    best_set = NULL
+
+    tag_vec = vector(length = length(input_x))
+
+    clr_vec = vector(length = length(input_x))
+
+    for(mlp_count in c(1:num_mlps))
+    {
+        w = rnorm(num_n_hid)
+
+        a = rnorm(num_n_hid, 0, a_stddev)
+
+        b = runif(num_n_hid, -2, 2)
+
+        tag_vec[] = mlp_count
+
+        clr_vec[] = 1
+
+        y_out = mlp(input_x, w, a, b)
+
+        err = 0.5 * sum((y_out - input_x)**2)
+
+        dataset = rbind(dataset, data.frame(xa = input_x, ya = y_out, group_id = tag_vec, colour_id = clr_vec))
+
+        if(is.null(min_err) || err < min_err)
+        {
+            best_set = y_out
+
+            min_err = err
+        }
+    }
+
+    tag_vec = num_mlps + 1
+
+    clr_vec[] = 2
+
+    g_y = -input_x
+
+    dataset = rbind(dataset, data.frame(xa = input_x, ya = g_y, group_id = tag_vec, colour_id = clr_vec))
+
+    tag_vec = num_mlps + 2
+
+    clr_vec[] = 3
+
+    dataset = rbind(dataset, data.frame(xa = input_x, ya = best_set, group_id = tag_vec, colour_id = clr_vec))
+
+    ggplot(dataset, aes(x = xa, y = ya, group = as.factor(group_id), colour = as.factor(colour_id))) + geom_line() + scale_color_manual(values = c("grey", "darkblue", "darkgreen"))
+
+    # ggplot(dataset, aes(x = xa, y = ya, group = as.factor(group_id))) + geom_line()
+
+    ggsave(file_path)
+}
+
+# question a and c.
 input_x = seq(-2, 2, 0.05)
 
-dataset = NULL
+run_test_and_plot(input_x, num_n_hid = 10, num_mlps = 50, a_stddev = 2, file_path = "./e2-2a.png")
 
-tag_vec = vector(length = length(input_x))
-
-clr_vec = vector(length = length(input_x))
-
-err = NULL
-
-min_err = NULL
-
-best_set = NULL
-
-for(mlp_count in c(1:50))
-{
-    w = rnorm(10)
-
-    a = rnorm(10, 0, 2)
-
-    b = runif(10, -2, 2)
-
-    tag_vec[] = mlp_count
-
-    clr_vec[] = 1
-
-    y_out = mlp(input_x, w, a, b)
-
-    err = 0.5 * sum((y_out - input_x)**2)
-
-    tmp_dataset = data.frame(x = input_x, y = y_out, group_id = tag_vec, colour_id = clr_vec)
-
-    dataset = rbind(dataset, tmp_dataset)
-
-    if(min_err == NULL || err < min_err)
-    {
-        best_set = y_out
-
-        min_err = err
-    }
-}
-
-tag_vec = 51
-
-clr_vec[] = 2
-
-g_y = -input_x
-
-dataset = rbind(dataset, data.frame(x = input_x, y = g_y, group_id =tag_vec, colour_id = clr_vec))
-
-tag_vec = 52
-
-clr_vec[] = 3
-
-dataset = rbind(dataset, data.frame(x = input_x, y = best_set, group_id = tag_vec, colour_id = clr_vec))
-
-ggplot(dataset, aes(x = x, y = y, group = as.factor(group_id), colour = as.factor(colour_id))) + geom_line() + scale_color_manual(values = c("grey", "blue", "green"))
-
-ggsave("./e2-2a.png")
-
-# question b.
-dataset = NULL
-
-tag_vec = vector(length = length(input_x))
-
-clr_vec = vector(length = length(input_x))
-
-for(mlp_count in c(1:50))
-{
-    w = rnorm(10)
-
-    a = rnorm(10, 0, 0.5)
-
-    b = runif(10, -2, 2)
-
-    tag_vec[] = mlp_count
-
-    clr_vec[] = 1
-
-    y_out = mlp(input_x, w, a, b)
-
-    dataset = rbind(dataset, data.frame(x = input_x, y = y_out, group_id = tag_vec, colour_id = clr_vec))
-}
-
-tag_vec = 51
-
-clr_vec[] = 2
-
-g_y = -input_x
-
-dataset = rbind(dataset, data.frame(x = input_x, y = g_y, group_id = as.factor(tag_vec), colour_id = as.factor(clr_vec)))
-
-ggplot(dataset, aes(x = x, y = y, group = group_id, colour = colour_id)) + geom_line() + scale_color_manual(values = c("grey", "blue"))
-
-ggsave("./e2-2b.png")
+# question b and c.
+run_test_and_plot(input_x, num_n_hid = 10, num_mlps = 50, a_stddev = 0.5, file_path = "./e2-2b.png")
