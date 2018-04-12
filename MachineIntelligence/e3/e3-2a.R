6cc9b4f008b0142c4550cae0ff051f204c8bb28a
CFWLoader <cfwloader@gmail.com>
2018-04-12 17:02:09 +0800

    Prepare for bias.

--- MachineIntelligence/e3/e3-2a.R
+++ MachineIntelligence/e3/e3-2a.R
@@ -33,19 +33,64 @@ create_mlp_random <- function(num_input, num_output, num_mlp_hidden)
     return(weights)
 }
 
-forward_propagation <- function(input, weights)
+# transfer_fun <- function(x)
+# {
+#     return(1/(1 + exp(-x)))
+# }
+transfer_fun <- tanh
+
+forward_propagation <- function(input, weights, transfer_funs)
 {
     layer_outputs = list()
+
+    layer_input = input
+
+    layer_index = 1
 
     for(weight_mat in weights)
     {
         # print(weight_mat)
-        
+
+        transfer_fun_mat = transfer_funs[[layer_index]]
+
+        layer_output = numeric(length = ncol(weight_mat))
+
+        layer_output[] = 0
+
+        for(input_idx in c(1:nrow(weight_mat)))
+        {
+            for(output_idx in c(1:ncol(weight_mat)))
+            {
+                # print(paste(input_idx, output_idx, weight_mat[input_idx, output_idx]))
+                
+                layer_output[output_idx] = layer_output[output_idx] + 
+                    weight_mat[input_idx, output_idx] * layer_input[input_idx]
+            }
+        }
+
+        for(output_idx in c(1:length(layer_output)))
+        {
+            layer_output[output_idx] = transfer_fun_mat[[output_idx]](layer_output[output_idx])
+        }
+
+        layer_input = layer_output
+
+        layer_outputs[[layer_index]] = layer_output
+
+        layer_index = layer_index + 1
     }
+
+    return(layer_outputs)
 }
 
 weights = create_mlp_random(1, 1, c(3))
 
-forward_propagation(training_set$V1, weights)
+transfer_funs = list()
 
-# print(weights)
+transfer_funs[[2]] = c(function(x){return(x)})
+
+transfer_funs[[1]] = c(transfer_fun, transfer_fun, transfer_fun)
+
+layer_outputs = forward_propagation(training_set$V1[1], weights, transfer_funs)
+
+print(layer_outputs)
