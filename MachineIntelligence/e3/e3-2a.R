9a8e2315c9d91304e21c755386ae6d049eecfe53
CFWLoader <cfwloader@gmail.com>
2018-04-13 15:45:07 +0800

    Forward propagation is workable. Now start coding back propagation.

--- MachineIntelligence/e3/e3-2a.R
+++ MachineIntelligence/e3/e3-2a.R
@@ -5,6 +5,14 @@ setwd(script.dir)
 
 training_set = read.csv("../datasets/RegressionData.txt", header = FALSE, sep = " ")
 
+ones_vec = rep(1, times = nrow(training_set));
+
+# ones_vec[] = 1
+
+x_mat = matrix(c(training_set$V1, ones_vec), ncol = 2)
+
+y_t = as.numeric(training_set$V2)
+
 generate_weight_matrix <- function(row, col)
 {
     return(matrix(runif(row * col, -0.5, 0.5), nrow = row, ncol  = col))
@@ -16,28 +24,22 @@ create_mlp_random <- function(num_input, num_output, num_mlp_hidden)
 
     weights <- list()
 
-    weights[[1]] <- generate_weight_matrix(num_input, num_mlp_hidden[1])
+    weights[[1]] <- generate_weight_matrix(num_input + 1, num_mlp_hidden[1])
 
     hidden_layer_index = 2
 
     while(hidden_layer_index <= num_hidden)
     {
-        weights[[hidden_layer_index]] <- generate_weight_matrix(num_mlp_hidden[hidden_layer_index - 1], 
+        weights[[hidden_layer_index]] <- generate_weight_matrix(num_mlp_hidden[hidden_layer_index - 1] + 1, 
             num_mlp_hidden[hidden_layer_index])
 
         hidden_layer_index = hidden_layer_index + 1
     }
 
-    weights[[num_hidden + 1]] <- generate_weight_matrix(num_mlp_hidden[num_hidden], num_output)
+    weights[[num_hidden + 1]] <- generate_weight_matrix(num_mlp_hidden[num_hidden] + 1, num_output)
 
     return(weights)
 }
-
-# transfer_fun <- function(x)
-# {
-#     return(1/(1 + exp(-x)))
-# }
-transfer_fun <- tanh
 
 forward_propagation <- function(input, weights, transfer_funs)
 {
@@ -47,33 +49,20 @@ forward_propagation <- function(input, weights, transfer_funs)
 
     layer_index = 1
 
+    ones_vec = rep(1, times = nrow(layer_input))
+
     for(weight_mat in weights)
     {
-        # print(weight_mat)
-
         transfer_fun_mat = transfer_funs[[layer_index]]
 
-        layer_output = numeric(length = ncol(weight_mat))
+        layer_output = layer_input %*% weight_mat
 
-        layer_output[] = 0
-
-        for(input_idx in c(1:nrow(weight_mat)))
+        for(output_idx in c(1:ncol(layer_output)))
         {
-            for(output_idx in c(1:ncol(weight_mat)))
-            {
-                # print(paste(input_idx, output_idx, weight_mat[input_idx, output_idx]))
-                
-                layer_output[output_idx] = layer_output[output_idx] + 
-                    weight_mat[input_idx, output_idx] * layer_input[input_idx]
-            }
+            layer_output[,output_idx] = transfer_fun_mat[[output_idx]](layer_output[,output_idx])
         }
 
-        for(output_idx in c(1:length(layer_output)))
-        {
-            layer_output[output_idx] = transfer_fun_mat[[output_idx]](layer_output[output_idx])
-        }
-
-        layer_input = layer_output
+        layer_input = cbind(layer_output, ones_vec)
 
         layer_outputs[[layer_index]] = layer_output
 
@@ -83,14 +72,25 @@ forward_propagation <- function(input, weights, transfer_funs)
     return(layer_outputs)
 }
 
+back_propagation <- function(input, layer_outputs, errors, weights)
+{
+    delta_weights = list()
+}
+
+transfer_fun <- tanh
+
 weights = create_mlp_random(1, 1, c(3))
 
 transfer_funs = list()
+
+# transfer_funs[[3]] = c(function(x){return(x)})
+
+# transfer_funs[[2]] = c(transfer_fun, transfer_fun, transfer_fun, transfer_fun)
 
 transfer_funs[[2]] = c(function(x){return(x)})
 
 transfer_funs[[1]] = c(transfer_fun, transfer_fun, transfer_fun)
 
-layer_outputs = forward_propagation(training_set$V1[1], weights, transfer_funs)
+layer_outputs = forward_propagation(x_mat, weights, transfer_funs)
 
-print(layer_outputs)
+print(sum((layer_outputs[[2]] - y_t)**2)/2)
