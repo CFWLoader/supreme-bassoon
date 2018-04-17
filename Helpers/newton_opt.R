a1b787f7c2e4b111e458a0dcdef5a69f0b2cf320
CFWLoader <cfwloader@gmail.com>
2018-04-17 15:06:27 +0800

    Step halving seems cannot save me.

--- Helpers/newton_opt.R
+++ Helpers/newton_opt.R
@@ -53,9 +53,11 @@ library(rjson)
 # 	# return([x[0] - deltax[0], x[1] - deltax[1]])
 # }
 
-newtonMO2V = function(firstDrv, secondDrv, initialVal, epsilon = 1e-13)
+newtonMO2V = function(llf, firstDrv, secondDrv, initialVal, epsilon = 1e-13)
 {
 	x = matrix(initialVal, nrow = 2, byrow = TRUE)
+
+	stepSize = 1
 
 	hessian = matrix(c(secondDrv[[1, 1]](x[1], x[2]), secondDrv[[1, 2]](x[1], x[2]), secondDrv[[2, 1]](x[1], x[2]), secondDrv[[2, 2]](x[1], x[2])),
         nrow = 2, byrow = TRUE)
@@ -66,30 +68,37 @@ newtonMO2V = function(firstDrv, secondDrv, initialVal, epsilon = 1e-13)
 
     deltax = hessianInv %*% jaccob
 
-    # print(x)
-
-    # print(hessian)
-
-    # print(hessianInv)
-
-    # print(jaccob)
-
-    # print("DeltaX:")
-
-	xn = c(x[1] - deltax[1], x[2] - deltax[2])
-
-    # print(deltax)
-
 	iterations = 0
 
-	while(abs(xn[1] - x[1]) > epsilon || abs(xn[2] - x[2]) > epsilon)
+	logLikeVal = llf(x[1], x[2])
+	
+	xn = x - stepSize * deltax
+
+	logLikeValNext = llf(xn[1], xn[2])
+
+	while(logLikeValNext < logLikeVal)
+	{
+		stepSize = stepSize / 2
+
+		xn = x - stepSize * deltax
+
+		logLikeValNext = llf(xn[1], xn[2])
+	}
+
+	# while(abs(logLikeValNext - logLikeVal) / (abs(logLikeVal) + 1e-8) > epsilon)
+	while(max(abs(xn - x) / (abs(x) + 1e-10)) > epsilon)
+	# while(abs(xn[1] - x[1]) > epsilon || abs(xn[2] - x[2]) > epsilon)
 	{
 		iterations = iterations + 1
+
+		stepSize = 1
+
+		logLikeVal = logLikeValNext
 
 		x = xn
 
 		hessian = matrix(c(secondDrv[[1, 1]](x[1], x[2]), secondDrv[[1, 2]](x[1], x[2]), secondDrv[[2, 1]](x[1], x[2]), secondDrv[[2, 2]](x[1], x[2])),
-        nrow = 2, byrow = TRUE)
+        	nrow = 2, byrow = TRUE)
 
 		hessianInv = ginv(hessian)
 
@@ -97,7 +106,18 @@ newtonMO2V = function(firstDrv, secondDrv, initialVal, epsilon = 1e-13)
 
 		deltax = hessianInv %*% jaccob
 
-		xn = c(x[1] - deltax[1], x[2] - deltax[2])
+		xn = x - stepSize * deltax
+
+		logLikeValNext = llf(xn[1], xn[2])
+
+		while(logLikeValNext < logLikeVal)
+		{
+			stepSize = stepSize / 2
+			
+			xn = x - stepSize * deltax
+
+			logLikeValNext = llf(xn[1], xn[2])
+		}
 	}
 
 	print(paste("Newton iterated ", iterations))
@@ -136,6 +156,11 @@ gammaParameters = function(data)
 	logMu = logMu / len;
 
 	# console.log("gp: len=" + len + ", log sum=" + logSumVal.toString() + ", sum=" + sumVal);
+
+	llf <- function(a, b)
+	{
+		return(len * a * log(b) - len * log(gamma(a)) + (a - 1) * logSumVal - b * sumVal)
+	}
 	
 	f1 <- function(a, b)
 	{
@@ -180,7 +205,7 @@ gammaParameters = function(data)
 
 	# var beta = alpha / mu;
 
-	return(newtonMO2V(c(f1, f2), matrix(c(f11, f12, f21, f22), nrow = 2, byrow = TRUE), c(alpha, beta)))
+	return(newtonMO2V(llf, c(f1, f2), matrix(c(f11, f12, f21, f22), nrow = 2, byrow = TRUE), c(alpha, beta)))
 	# return(newtonMO2V(f1, f2, f11, f12, f21, f22, c(alpha, beta)))
     
 }
