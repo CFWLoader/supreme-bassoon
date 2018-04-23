0acbfd1162cbd85079628ae4f88d29c82628cc17
CFWLoader <cfwloader@gmail.com>
2018-04-23 13:25:23 +0800

    Ruby version's Weib estimator.

--- Helpers/weib_est.rb
+++ Helpers/weib_est.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'matrix'
 
 module NewtonRaphson
 end
@@ -11,6 +12,20 @@ module Weib
     def self.loglikelihood(beta, gamma, source_data)
         
         weight = source_data.length
+
+        pow_gam_ln_sum = 0
+
+        ln_sum = 0
+
+        source_data.each {|val|
+
+            pow_gam_ln_sum += (val / a)**b * Math.log(val / a)
+
+            ln_sum += Math.log(val)
+
+        }
+
+        weight * (Math.log(b) - b * Math.log(a)) - pow_gam_ln_sum + ln_sum
 
     end
 
@@ -42,9 +57,97 @@ module Weib
     end
 
     def self.gradient
+
+        g1 = ->(a, b, data) {
+
+            pow_gam_sum = 0
+
+            data.each do |val|
+
+                pow_gam_sum += (val / a)**b
+
+            end
+            
+            (pow_gam_sum - data.length) * b / a
+
+        }
+
+        g2 = ->(a, b, data) {
+
+            pow_gamma_ln_sum = 0
+
+            ln_sum = 0
+
+            data.each { |val|
+
+                pow_gamma_ln_sum += (val / a)**b * Math.log(val / a)
+
+                ln_sum += Math.log(val)
+
+            }
+
+            data.length * (1 / b - Math.log(a)) - pow_gamma_ln_sum + ln_sum
+
+        }
+
+        [g1, g2]
+
     end
 
     def self.hessian
+
+        h11 = ->(a, b, data){
+
+            pow_gam_sum = 0
+
+            data.each {|val|
+
+                pow_gam_sum += (val / a) ** b
+
+            }
+
+            (data.length - (b + 1) * pow_gam_sum) * b / a**2
+
+        }
+
+        h12 = ->(a, b, data){
+
+            pow_gam_sum = 0
+
+            pow_gamma_ln_sum = 0
+
+            data.each {|val|
+
+                pow_gam_val = (val / a)**b
+
+                pow_gam_sum += pow_gam_val
+
+                pow_gamma_ln_sum += pow_gam_val * Math.log(val / a)
+
+            }
+
+            (-data.length + pow_gam_sum + b * pow_gamma_ln_sum) / a
+
+        }
+
+        h21 = h12
+
+        h22 = ->(a, b, data){
+
+            pow_gamma_ln2_sum = 0
+
+            data.each {|val|
+
+                pow_gamma_ln2_sum += (val / a)**b * Math.log(val / a)**2
+
+            }
+
+            -data.length / b**2 - pow_gamma_ln2_sum
+
+        }
+
+        [[h11, h12], [h21, h22]]
+
     end
 end
 
@@ -59,4 +162,24 @@ aqi_data = []
 end
 
 # Weib.estimate aqi_data
-print Weib.loglikelihoodOpt 1, 1, aqi_data
+# print Weib.loglikelihoodOpt 1, 1, aqi_data
+
+x = Matrix.column_vector([1, 1])
+
+gradient = Weib.gradient
+
+hessian_mat = Weib.hessian
+
+gradient_val = Matrix.column_vector([gradient[0].call(x[0, 0], x[1, 0], aqi_data), gradient[1].call(x[0, 0], x[1, 0], aqi_data)])
+
+hessian_val = Matrix[
+    [hessian_mat[0][0].call(x[0, 0], x[1, 0], aqi_data), hessian_mat[0][1].call(x[0, 0], x[1, 0], aqi_data)],
+    [hessian_mat[1][0].call(x[0, 0], x[1, 0], aqi_data), hessian_mat[1][1].call(x[0, 0], x[1, 0], aqi_data)]]
+
+hessian_inv_val = hessian_val.inv
+
+delta_x = hessian_inv_val * gradient_val
+
+xn = x - delta_x
+
+print xn
