1265aedd268cae07bbd98c10d3077fca67749f35
CFWLoader <cfwloader@gmail.com>
2018-04-23 14:59:55 +0800

    I might find out the problem.

--- Helpers/weib_est.rb
+++ Helpers/weib_est.rb
@@ -9,7 +9,7 @@ module Weib
         puts "Hello!"
     end
 
-    def self.loglikelihood(beta, gamma, source_data)
+    def self.loglikelihood(a, b, source_data)
         
         weight = source_data.length
 
@@ -19,13 +19,13 @@ module Weib
 
         source_data.each {|val|
 
-            pow_gam_ln_sum += (val / a)**b * Math.log(val / a)
+            pow_gam_ln_sum = pow_gam_ln_sum + ((val / a)**b)
 
-            ln_sum += Math.log(val)
+            ln_sum = ln_sum + Math.log(val)
 
         }
 
-        weight * (Math.log(b) - b * Math.log(a)) - pow_gam_ln_sum + ln_sum
+        weight * (Math.log(b) - b * Math.log(a)) - pow_gam_ln_sum + (b - 1) * ln_sum
 
     end
 
@@ -68,7 +68,7 @@ module Weib
 
             end
             
-            (pow_gam_sum - data.length) * b / a
+            (pow_gam_sum - data.length) * (b / a)
 
         }
 
@@ -80,13 +80,13 @@ module Weib
 
             data.each { |val|
 
-                pow_gamma_ln_sum += (val / a)**b * Math.log(val / a)
+                pow_gamma_ln_sum += ((val / a)**b) * Math.log(val / a)
 
                 ln_sum += Math.log(val)
 
             }
 
-            data.length * (1 / b - Math.log(a)) - pow_gamma_ln_sum + ln_sum
+            data.length * ((1 / b) - Math.log(a)) - pow_gamma_ln_sum + ln_sum
 
         }
 
@@ -106,7 +106,7 @@ module Weib
 
             }
 
-            (data.length - (b + 1) * pow_gam_sum) * b / a**2
+            (data.length - (b + 1) * pow_gam_sum) * (b / (a**2))
 
         }
 
@@ -138,11 +138,11 @@ module Weib
 
             data.each {|val|
 
-                pow_gamma_ln2_sum += (val / a)**b * Math.log(val / a)**2
+                pow_gamma_ln2_sum += ((val / a)**b) * (Math.log(val / a)**2)
 
             }
 
-            -data.length / b**2 - pow_gamma_ln2_sum
+            -(data.length / (b**2)) - pow_gamma_ln2_sum
 
         }
 
@@ -181,5 +181,70 @@ hessian_inv_val = hessian_val.inv
 delta_x = hessian_inv_val * gradient_val
 
 xn = x - delta_x
+
+llv = Weib.loglikelihood(x[0, 0], x[1, 0], aqi_data)
+
+llv_next = Weib.loglikelihood(xn[0, 0], xn[1, 0], aqi_data)
+
+step_size = 1
+
+while llv_next < llv do
+
+    step_size /= 2
+
+    xn = x - step_size * delta_x
+
+    llv_next = Weib.loglikelihood(xn[0, 0], xn[1, 0], aqi_data)
+
+end
+
+iteration = 1
+
+puts "Iteration: #{iteration}, LLVN: #{llv_next}"
+puts "Gra: #{gradient_val}, H: #{hessian_val}"
+puts "X(k+1): #{xn}"
+
+while iteration < 15 do 
+
+    iteration += 1
+
+    x = xn
+
+    llv = llv_next 
+
+    step_size = 1
+
+    gradient_val = Matrix.column_vector([gradient[0].call(x[0, 0], x[1, 0], aqi_data), gradient[1].call(x[0, 0], x[1, 0], aqi_data)])
+
+    hessian_val = Matrix[
+        [hessian_mat[0][0].call(x[0, 0], x[1, 0], aqi_data), hessian_mat[0][1].call(x[0, 0], x[1, 0], aqi_data)],
+        [hessian_mat[1][0].call(x[0, 0], x[1, 0], aqi_data), hessian_mat[1][1].call(x[0, 0], x[1, 0], aqi_data)]]
+
+    hessian_inv_val = hessian_val.inv
+
+    delta_x = hessian_inv_val * gradient_val
+
+    xn = x - delta_x
+
+    llv_next = Weib.loglikelihood(xn[0, 0], xn[1, 0], aqi_data)
+
+    step_size = 1
+
+    while llv_next < llv do
+
+        step_size /= 2
+
+        xn = x - step_size * delta_x
+
+        llv_next = Weib.loglikelihood(xn[0, 0], xn[1, 0], aqi_data)
+
+    end
+
+    puts "Iteration: #{iteration}, LLVN: #{llv_next}"
+    puts "Gra: #{gradient_val}, H: #{hessian_val}"
+    puts "X(k+1): #{xn}"
+
+end
+
 
 print xn
