ec112206aa4d68de17a7e588e15b74d805b5b884
CFWLoader <cfwloader@gmail.com>
2018-04-27 16:06:44 -0400

    Add cdf to validate.

--- Helpers/pareto_fit_val.R
+++ Helpers/pareto_fit_val.R
@@ -1,15 +1,28 @@
 library(ParetoPosStable)
+library(EnvStats)
 
 script.dir = dirname(sys.frame(1)$ofile)
 
 setwd(script.dir)
 
-aqi.data.frame = read.csv("../../csv_data/AQI.csv")
+# aqi.data.frame = read.csv("../../csv_data/AQI.csv")
 
-aqi.data = as.numeric(aqi.data.frame$AQI)
+# aqi.data = as.numeric(aqi.data.frame$AQI)
 
-print(min(aqi.data))
+# print(min(aqi.data))
 
 # print(head(aqi.data.frame$AQI))
 
 # print(pareto.fit(as.numeric(aqi.data.frame$AQI)))
+
+test_data <- c(1, 2, 3, 7, 10, 11)
+
+ft_val <- pareto.fit(test_data)
+
+alpha <- ft_val[[1]]$lambda
+
+loc <- ft_val[[1]]$sigma
+
+print(ppareto(4, loc, alpha))
+
+print(ppareto(5.66, loc, alpha))
