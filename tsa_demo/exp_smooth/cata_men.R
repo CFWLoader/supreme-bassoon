dcaab9aeb604385e1e367e130bddd099579381f7
CFWLoader <cfwloader@gmail.com>
2018-05-08 11:27:57 +0800

    Matched.

--- tsa_demo/exp_smooth/cata_men.R
+++ tsa_demo/exp_smooth/cata_men.R
@@ -18,6 +18,24 @@ init_level <- 11453.646871104995
 
 men.my.nn <- exp_sm_family.nn(alpha, init_level, men.obs)
 
-print(men.my.nn)
+# print(men.my.nn)
 
 # print(all.equal(men.mod.nn, men.my.nn))
+
+data.len <- length(men.obs)
+
+men.bkw.nn <- rep(length = data.len, 0)
+
+men.bkw.nn[data.len] <- (sm.val - alpha * men.obs[data.len]) / (1 - alpha)
+
+# print(men.bkw.nn[data.len])
+
+for(idx in c(data.len:2))
+{
+    st_1 <- (men.bkw.nn[idx] - alpha * men.obs[idx - 1]) / (1 - alpha)
+    men.bkw.nn[idx - 1] <- st_1
+}
+
+# print(men.bkw.nn)
+
+print(all.equal(men.mod.nn, men.bkw.nn))
