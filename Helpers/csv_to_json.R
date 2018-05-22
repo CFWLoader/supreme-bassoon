c99423df12a2b7ae05c886ca179c26fca8e86abd
CFWLoader <cfwloader@gmail.com>
2018-05-22 16:30:39 +0800

    Mark.

--- Helpers/csv_to_json.R
+++ Helpers/csv_to_json.R
@@ -4,12 +4,12 @@ script.dir <- dirname(sys.frame(1)$ofile)
 
 setwd(script.dir)
 
-input.df <- read.csv("../../va_data/catalog_seasfac_cus.csv")
+input.df <- read.csv("../tsa_demo/dataset/cata_men_na.csv")
 
 # print(str(input.df))
 
 # print(head(input.df$men))
 
-target.column <- as.numeric(input.df$men)
+target.column <- as.numeric(input.df$X.TS.men)
 
-write.table(toJSON(target.column), './cata_men.json', row.names = FALSE, col.names = FALSE, quote = FALSE)
+write.table(toJSON(target.column), './cata_men_na.json', row.names = FALSE, col.names = FALSE, quote = FALSE)
