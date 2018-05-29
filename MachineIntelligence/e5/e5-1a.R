e84640fa7055258059e8a80085a37e254c264d6c
CFWLoader <cfwloader@gmail.com>
2018-05-29 16:14:34 +0800

    Take a break.

--- MachineIntelligence/e5/e5-1a.R
+++ MachineIntelligence/e5/e5-1a.R
@@ -88,47 +88,73 @@ shpered_x <- t(shpered_x)
 origin.df <- data.frame(
     x1 = train_x[, 1],
     x2 = train_x[, 2],
+    y = train_set.df$obs,
     grp_id = 1
 )
 
 origin.df <- rbind(origin.df, data.frame(
     x1 = mean(train_x[, 1]),
     x2 = mean(train_x[, 2]),
+    y = 0,
     grp_id = 2
 ))
 
-ggplot(origin.df, aes(x = x1, y = x2, color = as.factor(grp_id))) + geom_point()
+ggplot(origin.df, aes(x = x1, y = x2, color = y, shape = as.factor(grp_id))) + geom_point() + scale_color_gradientn(colors = rainbow(50))
 
 ggsave("./e5-1a-origin.png")
 
 centered.df <- data.frame(
     x1 = centered_x[, 1],
     x2 = centered_x[, 2],
+    y = train_set.df$obs,
     grp_id = 1
 )
 
 centered.df <- rbind(centered.df, data.frame(
     x1 = mean(centered_x[, 1]),
     x2 = mean(centered_x[, 2]),
+    y = 0,
     grp_id = 2
 ))
 
-ggplot(centered.df, aes(x = x1, y = x2, color = as.factor(grp_id))) + geom_point()
+ggplot(centered.df, aes(x = x1, y = x2, color = y, shape = as.factor(grp_id))) + geom_point() + scale_color_gradientn(colors = rainbow(50))
 
 ggsave("./e5-1a-centered.png")
 
 shpered.df <- data.frame(
     x1 = shpered_x[, 1],
     x2 = shpered_x[, 2],
+    y = train_set.df$obs,
     grp_id = 1
 )
 
 shpered.df <- rbind(shpered.df, data.frame(
     x1 = mean(shpered_x[, 1]),
     x2 = mean(shpered_x[, 2]),
+    y = 0,
     grp_id = 2
 ))
 
-ggplot(shpered.df, aes(x = x1, y = x2, color = as.factor(grp_id))) + geom_point() # + xlim(-1, 1) + ylim(-1, 1)
+ggplot(shpered.df, aes(x = x1, y = x2, color = y, shape = as.factor(grp_id))) + geom_point() + scale_color_gradientn(colors = rainbow(50)) # + xlim(-1, 1) + ylim(-1, 1)
 
 ggsave("./e5-1a-shpered.png")
+
+ggplot(validate_set.df, aes(x = x1, y = x2, color = dens)) + geom_point() + scale_color_gradientn(colors = rainbow(50)) # + xlim(-1, 1) + ylim(-1, 1)
+
+ggsave("./e5-1a-validate.png")
+
+valid_x <- t(data.matrix(validate_set.df[c(1, 2)]))
+
+centered_val_x <- center_data.matrix(valid_x)
+
+sphered_val_x <- eigenvalue.diag %*% t(x_cov.eigenvectors) %*% centered_val_x
+
+sphered_val.df <- data.frame(
+    x1 = sphered_val_x[1, ],
+    x2 = sphered_val_x[2, ],
+    y = validate_set.df$dens
+)
+
+ggplot(sphered_val.df, aes(x = x1, y = x2, color = y)) + geom_point() + scale_color_gradientn(colors = rainbow(50)) # + xlim(-1, 1) + ylim(-1, 1)
+
+ggsave("./e5-1a-validate-shpered.png")
---
+++ MachineIntelligence/e5/e5-1b.R
@@ -0,0 +1,65 @@
+library(ggplot2)
+library(MASS)
+
+script.dir <- dirname(sys.frame(1)$ofile)
+
+setwd(script.dir)
+
+train_set.df <- read.csv("../datasets/TrainingRidge.csv")
+
+validate_set.df <- read.csv("../datasets/ValidationRidge.csv")
+
+x1 <- train_set.df$x1
+
+x2 <- train_set.df$x2
+
+x1.val <- validate_set.df$x1
+
+x2.val <- validate_set.df$x2
+
+monomials.df <- NULL
+
+valid.mono.df <- NULL
+
+monomial_lim <- 3
+
+for(k in c(0 : monomial_lim))
+{
+    for(a in c(0 : k))
+    {
+        b <- k - a
+        tmp.df <- data.frame(data = x1**a * x2**b)
+        monomials.df <- rbind(monomials.df, data.frame(
+            data = t(tmp.df),
+            row.names = c(paste("x1^", a, "*x2^", b, sep = ""))
+        ))
+        tmp.df <- data.frame(data = x1.val**a * x2.val**b)
+        valid.mono.df <- rbind(valid.mono.df, data.frame(
+            data = t(tmp.df),
+            row.names = c(paste("x1^", a, "*x2^", b, sep = ""))
+        ))
+    }
+}
+
+y_t <- data.matrix(train_set.df$obs)
+
+phi_mat <- data.matrix(monomials.df)
+
+phi_mat.tinv <- ginv(phi_mat %*% t(phi_mat))
+
+expand_val_x <- data.matrix(valid.mono.df)
+
+# weight <- solve(phi_mat %*% t(phi_mat)) %*% phi_mat %*% t(y_t)
+weight <- phi_mat.tinv %*% phi_mat %*% y_t
+
+y_prd <- t(weight) %*% expand_val_x 
+
+valid_prd.df <- data.frame(
+    x1 = x1.val,
+    x2 = x2.val,
+    yp = t(y_prd)
+)
+
+ggplot(valid_prd.df, aes(x = x1, y = x2, color = yp)) + geom_point() + scale_color_gradientn(colors = rainbow(50)) # + xlim(-1, 1) + ylim(-1, 1)
+
+ggsave("./e5-1b-val-predict.png")
