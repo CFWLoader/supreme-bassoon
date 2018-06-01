a53724d5d3cd296cad5dcd19a537d57472710ff2
CFWLoader <cfwloader@gmail.com>
2018-06-01 09:40:51 +0800

    Algo framework doesn't work.

--- MachineIntelligence/e5/e5-1c.R
+++ MachineIntelligence/e5/e5-1c.R
@@ -19,9 +19,11 @@ whiten_train.df <- data.frame(
     y = train_set.df$obs
 )
 
-fold_ready.df = train_set.df
+fold_ready.df = whiten_train.df
 
 k_fold <- 10
+
+monomial_lim <- 9
 
 pack_size <- floor(nrow(whiten_train.df) / k_fold)
 
@@ -34,31 +36,32 @@ for(k in c(1:k_fold))
     fold_t <- fold_ready.df[c(-start_idx : -end_idx), ]
     fold_val <- fold_ready.df[c(start_idx : end_idx), ]
     folded_pack[[k]] <- list(train = fold_t, valid = fold_val)
+    folded_pack[[k]]$train_exp <- monomial2dim(fold_t$x1, fold_t$x2, monomial_lim)
+    folded_pack[[k]]$valid_exp <- monomial2dim(fold_val$x1, fold_val$x2, monomial_lim)
+    folded_pack[[k]]$valid_exp.mat <- data.matrix(folded_pack[[k]]$valid_exp)
 }
 
-print(head(folded_pack[[2]]$valid))
+mse.df <- NULL
 
-# monomial_lim <- 10
+for(lam in seq(-4, 4, 0.1))
+{
+    resi.vec <- c()
+    for(k in c(1:k_fold))
+    {
+        weight <- retrieve_weight(folded_pack[[k]]$train_exp, folded_pack[[k]]$train$y, 10**lam)
+        y_prd <- t(weight) %*% folded_pack[[k]]$valid_exp.mat
+        residuals <- folded_pack[[k]]$valid$y - y_prd
+        resi.vec <- c(resi.vec, mean(residuals**2))
+    }
+    mse.df <- rbind(mse.df,
+        data.frame(
+            lambda = lam,
+            mse.val = mean(resi.vec),
+            std = sqrt(var(resi.vec))
+        )
+    )
+}
 
-# train.mono.df <- monomial2dim(x1, x2, monomial_lim)
+ggplot(mse.df, aes(x = lambda, y = mse.val)) + geom_line() + geom_errorbar(aes(min = mse.val - std, max = mse.val + std))
 
-# retrieve_weight(train.mono.df, train_set.df$obs, -4)
-
-# mse.df <- NULL
-
-# for(lam in seq(-4, 4, 0.1))
-# {
-#     weight <- retrieve_weight(train.mono.df, train_set.df$obs, 10**lam)
-#     y_prd <- t(weight) %*% expand_val_x 
-#     mse <- mean((y_prd - validate_set.df$dens)**2)
-#     mse.df <- rbind(mse.df,
-#         data.frame(
-#             lambda = lam,
-#             mse.val = mse
-#         )
-#     )
-# }
-
-# ggplot(mse.df, aes(x = lambda, y = mse.val)) + geom_line()
-
-# ggsave("./e5-1c-mse.png")
+ggsave("./e5-1c-mse.png")
