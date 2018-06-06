1af6aff3e44fb4353158dc85c232a7f8657ef4e0
CFWLoader <cfwloader@gmail.com>
2018-06-06 17:11:31 +0800

    Work comes.

---
+++ MachineIntelligence/e6/e6-1d.R
@@ -0,0 +1,45 @@
+library(keras)
+library(ggplot2)
+
+script.dir <- dirname(sys.frame(1)$ofile)
+setwd(script.dir)
+
+mnist <- dataset_mnist()
+
+# reshape
+# x_train <- array_reshape(mnist$train$x, c(nrow(mnist$train$x), 784))
+# x_test <- array_reshape(mnist$test$x, c(nrow(mnist$test$x), 784))
+# rescale
+# x_train <- x_train / 255
+# x_test <- x_test / 255
+
+# y_train <- to_categorical(mnist$train$y, 10)
+# y_test <- to_categorical(mnist$test$y, 10)
+
+model <- keras_model_sequential()
+
+model %>% 
+    layer_conv_2d(kernel_size = c(5, 5), input_shape = c(28, 28, 1))
+
+summary(model)
+
+# model %>% compile(
+#   loss = 'categorical_crossentropy',
+#   optimizer = optimizer_adam(lr = 0.001, beta_1 = 0.001, beta_2 = 0.999, epsilon = 10e-8),
+#   metrics = c('accuracy')
+# )
+
+# history <- model %>% fit(
+#   x_train, y_train,
+#   epochs = 10, batch_size = 100,
+# )
+
+# plt.df <- data.frame(
+#   x = c(1:length(history$metrics$loss)),
+#   loss = history$metrics$loss,
+#   acc = history$metrics$acc
+# )
+
+# ggplot(plt.df, aes(x = x)) + geom_point(aes(y = loss, color = "Loss")) + geom_point(aes(y = acc, color = "Acc")) + labs(color = "Type")
+
+# ggsave("e6-1d.png")
