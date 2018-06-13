f081bf660f003df9c815219d74894d77895ff624
CFWLoader <cfwloader@gmail.com>
2018-06-13 17:45:39 +0800

    Solved sheet 7.

--- MachineIntelligence/e7/e7-4b.R
+++ MachineIntelligence/e7/e7-4b.R
@@ -1,4 +1,3 @@
-library(stats)
 library(proxy)
 library(ggplot2)
 
@@ -13,8 +12,6 @@ philize <- function(xmat, centers, sigmak = 0.5)
 {
     distmat <- dist(centers, xmat, method = pr_DB$get_entry("Euclidean"))
     densmat <- exp(-distmat**2/(2*sigmak))
-    # densrs <- rowSums(densmat)
-    # normdens <- densmat / densrs
     rbind(rep(1, length = ncol(densmat)), densmat)
 }
 
@@ -27,28 +24,27 @@ retrieve_weight <- function(xmat, centers, yt, sigmak = 0.5)
 
 samsize <- 60
 
-trainset <- genclass(samsize, cls = 0)
-trainset <- rbind(trainset, genclass(samsize, mu1 = c(0, 0), mu2 = c(1, 1),  cls = 1))
+for(sigg in seq(0.1, 0.5, 0.1)){
+    trainset <- genclass(samsize, sigma = diag(2) * sigg, cls = 0)
+    trainset <- rbind(trainset, genclass(samsize, mu1 = c(0, 0), mu2 = c(1, 1), sigma = diag(2) * sigg, cls = 1))
 
-trainset_x <- data.matrix(trainset[c(1, 2)])
+    trainset_x <- data.matrix(trainset[c(1, 2)])
 
-testset <- make_plotpoint(trainset)
+    testset <- make_plotpoint(trainset)
 
-for(kval in c(10:20))
-{
-    for(sigmaval in seq(0.01, 1, 0.1))
+    for(sigmaval in c(0.45))
     {
-        kcenters <- data.matrix(kmeans(trainset_x, 2)$centers)
+        kcenters <- matrix(c(0, 0, 1, 1), nrow = 2, byrow = TRUE)
         weights <- retrieve_weight(trainset_x, kcenters, trainset$y, sigmaval)
         phiedtest <- philize(testset, kcenters, sigmaval)
         prd <- t(weights) %*% phiedtest
         prdlbl <- rep(1, length = length(prd))
         diffidx <- which(prd < 0)
-        if(length(diffidx) > 0)
-        {
-            prdlbl[diffidx] <- 0
-            test.pltdf <- data.frame(x1 = testset[, 1], x2 = testset[, 2], y = prdlbl, prob = 1)
-            dbplot(trainset, test.pltdf, sprintf("e7-4a-k%d-%2f.png", kval, sigmaval))
-        }
+        prdlbl[diffidx] <- 0
+        test.pltdf <- data.frame(x1 = testset[, 1], x2 = testset[, 2], y = prdlbl, prob = 1)
+        dbplot(trainset, test.pltdf, sprintf("e7-4b-sk%2f-sig%f.png", sigmaval, sigg))
+        tphi.pltdf <- data.frame(x1 = phiedtest[2, ], x2 = phiedtest[3, ], y = prdlbl)
+        ggplot(tphi.pltdf, aes(x = x1, y = x2, color = as.factor(y))) + geom_point()
+        ggsave(sprintf("e7-4b-phi-sk%2f-sig%f.png", sigmaval,sigg))
     }
 }
