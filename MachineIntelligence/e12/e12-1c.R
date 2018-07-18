fc3c650cd832a9f6ac1447d2003697df1cb8d5ca
cfwloader <cfwloader@gmail.com>
2018-07-18 19:15:49 +0800

    Continue.

--- MachineIntelligence/e12/e12-1c.R
+++ MachineIntelligence/e12/e12-1c.R
@@ -3,4 +3,21 @@ script.dir <- dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 
 source("./lib/loadmaze.R")
+source("./lib/tranmodel.R")
 source("./lib/analyticvalue.R")
+
+file_path <- "../datasets/mazes.txt"
+
+mazes <- loadmaze(file_path)
+
+actions <- c("up", "left", "right", "down")
+
+tranmodel <- gen_tranmodel(mazes[[1]], actions)
+
+rewards <- generate_rewards(actions, tranmodel$states)
+
+policies <- generate_uniform_policy(actions, tranmodel$states)
+
+str(rewards)
+
+str(policies)
--- MachineIntelligence/e12/lib/analyticvalue.R
+++ MachineIntelligence/e12/lib/analyticvalue.R
@@ -1,3 +1,39 @@
+generate_rewards <- function(actions, states)
+{
+    num_acts <- length(actions)
+    num_states <- length(states)
+
+    rewards <- matrix(0, nrow = num_states, ncol = num_acts)
+    
+    for(sidx in 1:num_states)
+    {
+        for(aidx in 1:num_acts)
+        {
+            rewards[sidx, aidx] <- ifelse(states[sidx] == "X", 1, 0)
+        }
+    }
+
+    rewards
+}
+
+generate_uniform_policy <- function(actions, states)
+{
+    num_acts <- length(actions)
+    num_states <- length(states)
+
+    rewards <- matrix(0, nrow = num_states, ncol = num_acts)
+    
+    for(sidx in 1:num_states)
+    {
+        for(aidx in 1:num_acts)
+        {
+            rewards[sidx, aidx] <- 1/num_acts
+        }
+    }
+
+    rewards
+}
+
 analyticvalue <- function(maze)
 {
 
