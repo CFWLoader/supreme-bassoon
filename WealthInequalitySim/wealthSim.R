60620f1c525f0b72d5c2d55619e2b3b94b067b15
cfwloader <cfwloader@gmail.com>
2019-04-09 22:01:44 +0800

    Now can do simple simulation.

--- WealthInequalitySim/wealthSim.R
+++ WealthInequalitySim/wealthSim.R
@@ -13,14 +13,29 @@ plotRanks <- function(playerWealth, filePath, sortRanks = TRUE){
 script.dir <- dirname(sys.frame(1)$ofile)
 setwd(script.dir)
 
-numPlayer = 100                 # 参与模拟的人数
-initWealth = 5000               # 参与人员初始财富值
-maxIterations = 1700            # 模拟的最大迭代数
+numPlayer = 1000                 # 参与模拟的人数
+initWealth = 50000               # 参与人员初始财富值
+maxIterations = 17000            # 模拟的最大迭代数
 
 wealthArr = rep.int(initWealth, times = numPlayer)
 plotRanks(wealthArr, "./simPlots/SimIter0.png")
 
-for(iter in 1:1700)
+for(iter in 1:maxIterations)
 {
-    print(iter)
+    # 一轮迭代中财富交换次数可以有什么模拟方法？目前暂且假定会发生{人数/4}次交易，即影响一半人左右
+    # （因为考虑产生随机数可能发生自己与自己重叠）。
+    wealthExchangeTimes = numPlayer / 4
+    for(exchangeCnt in 1 : wealthExchangeTimes){
+        payerId = runif(1, min = 1, max = numPlayer)
+        payeeId = runif(1, min = 1, max = numPlayer)
+        if(payerId != payeeId)
+        {
+            wealthArr[payeeId] = wealthArr[payeeId] + 1000
+            wealthArr[payerId] = wealthArr[payerId] - 1000
+        }
+    }
+    if(iter %% 1000 == 0){
+    iterFilePath = sprintf("./simPlots/SimIter%d.png", iter)
+    plotRanks(wealthArr, iterFilePath)
+    }
 }
