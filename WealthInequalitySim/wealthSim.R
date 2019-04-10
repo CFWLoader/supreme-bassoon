f20e45118a141084a0fb0d640e937e0e739cc3f6
cfwloader <cfwloader@gmail.com>
2019-04-10 11:43:48 +0800

    Now can plot info i need.

--- WealthInequalitySim/wealthSim.R
+++ WealthInequalitySim/wealthSim.R
@@ -50,6 +50,9 @@ for(iter in 1:maxIterations)
             # 交易额产生方式
             # dealSum = rnorm(1, mean = initWealth / 4, sd = initWealth * 0.01)
             dealSum <- as.integer(rlnorm(1, meanlog = log(initWealth * 0.001), sdlog = 1.6))
+            # if(wealthArr[payerId] < initWealth * -0.01 || wealthArr[payerId] - dealSum < initWealth * -0.01){
+            #     next
+            # }
             wealthArr[payeeId] <- wealthArr[payeeId] + dealSum
             wealthArr[payerId] <- wealthArr[payerId] - dealSum
         }
@@ -63,8 +66,51 @@ for(iter in 1:maxIterations)
     # }
 }
 
-bisplt.df <- data.frame(iteration = seq(1, maxIterations, 1), bisectQuantile = bisQuantiles)
+bisdata1.df <- data.frame(iteration = seq(1, maxIterations, 1), bisectQuantile = bisQuantiles, simId = 1)
 
-ggplot(bisplt.df) + geom_line(aes(x= iteration, y = bisectQuantile))
+wealthArr = rep.int(initWealth, times = numPlayer)
+
+bisQuantiles2 <- c()
+
+for(iter in 1:maxIterations)
+{
+    # 一轮迭代中财富交换次数可以有什么模拟方法？目前暂且假定会发生{人数/4}次交易，即影响一半人左右
+    # （因为考虑产生随机数可能发生自己与自己重叠）。
+    wealthExchangeTimes <- numPlayer / 4
+    for(exchangeCnt in 1 : wealthExchangeTimes){
+        payerId <- runif(1, min = 1, max = numPlayer)
+        payeeId <- runif(1, min = 1, max = numPlayer)
+        if(payerId != payeeId)
+        {
+            # 交易额产生方式
+            # dealSum = rnorm(1, mean = initWealth / 4, sd = initWealth * 0.01)
+            dealSum <- as.integer(rlnorm(1, meanlog = log(initWealth * 0.001), sdlog = 1.6))
+            # 加入弱势保护
+            if(wealthArr[payerId] < initWealth * -0.01 || wealthArr[payerId] - dealSum < initWealth * -0.01){
+                next
+            }
+            wealthArr[payeeId] <- wealthArr[payeeId] + dealSum
+            wealthArr[payerId] <- wealthArr[payerId] - dealSum
+        }
+    }
+    sortedRanks <- sort(wealthArr)
+    bisPoint <- searchBisectPoint(sortedRanks, bisectTotalWealth)
+    bisQuantiles2 <- c(bisQuantiles2, bisPoint)
+    # if(iter %% 1000 == 0){
+    #     iterFilePath <- sprintf("./simPlots/SimIter%d.png", iter)
+    #     plotRanks(sortedRanks, iterFilePath)
+    # }
+}
+
+bisdata2.df <- data.frame(iteration = seq(1, maxIterations, 1), bisectQuantile = bisQuantiles2, simId = 2)
+
+bisplt.df <- rbind(bisdata1.df, bisdata2.df)
+
+ggplot(bisplt.df) + geom_line(aes(x= iteration, y = bisectQuantile, color = factor(simId))) + 
+    xlab("迭代数") + ylab("均分总财富点") + ggtitle("财富分配模拟") + theme(plot.title = element_text(hjust = 0.5)) +
+    scale_color_manual(values=c("blue", "red"), 
+                       name="模拟类型",
+                       breaks=c(1, 2),
+                       labels=c("无保护", "弱势保护"))
 
 ggsave("./simPlots/quatiles.png")
